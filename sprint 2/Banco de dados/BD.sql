83f660c5855f8940e95f6c2f29538ca3d62a8a58
MarcceloSpinelli <marcelo.spinelli@sptech.school>
2023-04-23 20:25:41 -0300

    versão final

--- sprint 2/Banco de dados/BD.sql
+++ sprint 2/Banco de dados/BD.sql
@@ -32,15 +32,16 @@ status_sensor varchar(15)
 
 create table leitura_sensor (
 idLeitura_sensor int auto_increment,
-valores decimal(10, 2),
+temperatura decimal(10, 2),
+umidade decimal(10, 2),
 tipo_leitura varchar(45),
+DtLeitura_sensor datetime,
 fkSensor int,
 constraint pkComposta2 primary key (idLeitura_sensor, fkSensor)
 );
 
 create table tipo_sensor (
 idTipo_sensor int primary key auto_increment,
-tipo varchar(45),
 modelo varchar(45),
 metrica_leitura1 varchar(45),
 metrica_leitura2 varchar(45)
@@ -83,13 +84,59 @@ alter table parametros_local add column fkLoc INT,
 	add constraint fkLocRecursivaTipo foreign key (fkLoc)
 		references locais (idLocal);
         
-alter table sensores add column fkLoc int,
-	add constraint fkLocRecursivaSensor foreign key (fkLoc)
-		references locais (idLocal);
+alter table sensores add column fkEmpresa int,
+	add constraint fkEmpresaRecursivaSensor foreign key (fkEmpresa)
+		references empresa (idEmpresa);
 
 alter table sensores add column fkTipo int,
 	add constraint fkSensorTipoRecursiva foreign key (fkTipo)
 		references tipo_sensor (idTipo_sensor);
 
-alter table leitura_sensor add column umidade DECIMAL (10,2);
-alter table leitura_sensor add column temperatura DECIMAL (10,2);
+desc empresa;
+
+insert into empresa values
+(null, 'ValleysTech', 'Valleys Tech', '12345671234567', 'Henrique', 'mainemail@valleystech.com', 'vt123456789');
+
+desc usuario;
+
+insert into usuario values 
+(null, 'Fernanda', 'Caramico', '***********', '11912345678', 'fernanda.caramico@valleystech.com', 'vtCaramico', 'ativo', 1);
+
+desc locais;
+
+insert into locais values
+(null, 'SPTECH', 1);
+
+desc endereco;
+
+insert into endereco values
+(null, '123456789', 'Cerqueira César', 'SP', 'São Paulo', 'Rua Haddock Lobo', '595', 1);
+
+desc tipo_sensor;
+
+insert into tipo_sensor values
+(null, 'DHT11', 'temperatura', 'umidade');
+
+desc sensores;
+
+insert into sensores values
+(null, 'ativo', 1, 1);
+
+desc leitura_sensor;
+
+insert into leitura_sensor values
+(null, 25.00, 65.00, 'temp/umi', '2023-04-23 14:30:00', 1);
+
+desc parametros_local;
+
+insert into parametros_local values
+(null, 20.00, 30.00, 1, 1);
+
+select * from empresa
+join usuario on empresa.idEmpresa = usuario.fkEmpresa
+join sensores on sensores.fkEmpresa = empresa.idEmpresa
+join leitura_sensor on leitura_sensor.fkSensor = sensores.idSensores
+join tipo_sensor on tipo_sensor.idTipo_sensor = sensores.fkTipo
+join locais on locais.fkEmpresa = empresa.idEmpresa
+join endereco on endereco.fkloc = locais.idLocal
+join parametros_local on parametros_local.fkLoc = locais.idLocal;
